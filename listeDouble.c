84005ecbd942b68279b1a81ba89d9d92725048e8
Romain Fournet <fournet.romain79@gmail.com>
2025-03-25 14:33:41 +0100

    chore: init

---
+++ listeDouble.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "listeDouble.h"
+
+
+//initListe ne fait pas de malloc, juste une initialisation � NULL du pointeur de liste
+void initListe(T_liste *l){
+*l=NULL;
+}
+
+
+bool listeVide( T_liste l){
+    return (l==NULL);
+}
+
+void afficheListe( T_liste l){
+    T_liste courant = l;
+    while (courant!=NULL){
+        afficheStation(courant->pdata);  //fourni par types.h
+        courant=courant->suiv;
+    }
+}
+
+T_liste ajoutEnTete(T_liste l, Tstation* mydata){
+    T_liste nouv = (T_liste)malloc(sizeof(struct T_cell));
+
+    //CHANGEMENT par rapport � la partie2 d'updago
+    //nouv->pdata = (int*)malloc(sizeof(int)); ATTENTION PLUS DE RECOPIE de mydata, donc plus d'allocation m�moire sur ce champ
+
+    //SIMPLE BRANCHEMENT EN MEMOIRE ENTRE POINTEURS (SUR LA STATION EXISTANTE)
+    nouv->pdata=mydata;
+
+    if (l==NULL) // on cree en fait la premiere cellule de la liste
+    {
+        nouv->suiv = NULL;  //s�curise la pr�sence de NULL sur le champ suiv, au cas o� le ptr l n'aurait pas �t�  initialis� correctement via initListe
+        nouv->prec = NULL;
+    }
+    else  // la lste n'etait pas vide, on doit donc faire les branchements
+    {
+        nouv->suiv = l;
+        nouv->prec = NULL;
+        l->prec = nouv;
+    }
+    return nouv;
+}
+
+T_liste ajoutEnFin(T_liste l, Tstation* mydata){
+    T_liste nouv, courant=l;  //remarque: pas de malloc si on appelle ajoutEnTete
+
+    if (l==NULL) // on cree en fait la premiere cellule de la liste
+    {
+        return ajoutEnTete(l, mydata);
+    }
+    else  // la liste n'etant pas vide,d�placment sur la derniere cellule, malloc et branchements
+    {
+        while (courant->suiv != NULL){
+            courant=courant->suiv;
+        }
+        nouv = (T_liste)malloc(sizeof(struct T_cell));
+        nouv->pdata=mydata;
+
+        nouv->suiv = NULL;
+        nouv->prec = courant;
+        courant->suiv = nouv;
+    }
+    return l;  //la t�te d'origine, qui n'a pas chang�
+}
+
+Tstation* getPtrData(T_liste l){
+    if (l==NULL)
+    {
+        printf("\nERREUR ptr pdata non alloué");
+        return NULL;
+    }
+    else
+    {
+        return l->pdata;
+    }
+}
+
+T_liste getNextCell(T_liste l){
+    if (l==NULL)
+    {
+        return NULL;  //convention
+    }
+    else{
+        return l->suiv;
+    }
+}
+
+T_liste getPrevCell(T_liste l){
+    if (l==NULL)
+    {
+        return NULL;  //convention
+    }
+    else{
+        return l->prec;
+    }
+}
+
+//A vous la suite si besoin
---
+++ types.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "types.h"
+
+void afficheStation( Tstation *station){
+    if (station == NULL){
+        printf("\nerreur station non allouee!\n");
+    }
+    else{
+        if (station->arret_ou_troncon == ARRET){
+            printf("\nSTATION idStation: %d, Station : %s, posX = %d, posY = %d", station->idStation, station->nomStation, station->posX, station->posY);
+        }
+        else{ //alors le noeud est un troneon
+            printf("\n\nTRONCON idLigneBus: %d, temps: %d sec, distance e parcourir: %d metres", station->idLigneBus, station->coutTemps, station->coutDistance);
+            afficheStation(station->depart);
+            afficheStation(station->arrivee);
+            printf("\nfin troneon\n");
+        }
+    }
+}
+
+// Getteurs
+
+int getIdStation( Tstation *myStation){
+    return myStation->idStation;
+}
+
+char *getNomStation( Tstation *myStation){
+    return myStation->nomStation;
+}
+
+int getPosXStation( Tstation *myStation ){
+    return myStation->posX;
+}
+int getPosYStation( Tstation *myStation ){
+    return myStation->posY;
+}
+
+int getIdLigneTroncon(Tstation *myStation){
+    //une station n'est pas liee e une ligne, seulement le troncon
+    if (getTypeNoeud(myStation)==TRONCON)
+        return myStation->idLigneBus;
+    else{
+        printf("\n(getIdLigneTroncon) Erreur algo, vous n etes pas sur un troncon");
+        return -1;
+    }
+}
+
+TypeNoeud getTypeNoeud(Tstation *myStation){
+    return myStation->arret_ou_troncon;
+}
+
+
+
+int getPosXBus( Tbus myBus ){
+    return myBus->posXBus;
+}
+int getPosYBus( Tbus myBus ){
+    return myBus->posYBus;
+}
+int getIdBus( Tbus myBus ){
+    return myBus->idBus;
+}
+int getIdLigneActuelleDuBus( Tbus myBus ){
+    return myBus->idLigneBusActuelle;
+}
+
+int getIdLigneBus(Tbus myBus){
+    return myBus->idLigneBusActuelle;
+}
+
+TsensParcours getSensParcours(Tbus myBus){
+    return myBus->sensParcours;
+}
+
+
+
+// Setteurs
+
+void setActualStation( Tbus myBus, TlisteStation arrivalStation ){
+    myBus->positionSurLaLigneDeBus = arrivalStation;
+}
+
+TlisteStation getActualStation( Tbus myBus ){
+    return myBus->positionSurLaLigneDeBus;
+}
+
+void setPosXBus(Tbus myBus, int newX){
+    myBus->posXBus = newX;
+}
+
+void setPosYBus(Tbus myBus, int newY){
+    myBus->posYBus = newY;
+}
+
+void setIdLigneBus(Tbus myBus, int idLigne){
+    myBus->idLigneBusActuelle = idLigne;
+}
+
+void setSensParcours(Tbus myBus, TsensParcours sens ){
+    myBus->sensParcours = sens;
+}
+
+void setPositionSurLaLigneDeBus( Tbus myBus, TlisteStation myStation){
+    myBus->positionSurLaLigneDeBus = myStation;
+}
